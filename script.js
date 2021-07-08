d7f46af95f0e0bba94d81aa92db90a49464d988a
minh-ctrl <binhminh140899@gmail.com>
2021-07-09 04:27:22 +0900

    dynamically created grid and random color

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let options= window.prompt("how many blocks do you want? please choose from 10-64")
-const block= document.createElement('block');
+const container= document.querySelector('.container')
+function create_grid(){
+    let options= prompt("how many blocks do you want? please choose from 10-64")
+    let x= options * options;
+    document.documentElement.style.setProperty("--columns-row", options);
+    for (let i = 0; i < x; i++){
+        const block= document.createElement('div');
+        block.classList.add('block__RGB');
+        container.appendChild(block);
+        block.addEventListener('mouseover',()=>{
+            let color= Math.floor(Math.random()*16777215).toString(16) ;
+            block.style.backgroundColor='#' + color;
+        })
+    }}
+    create_grid();
